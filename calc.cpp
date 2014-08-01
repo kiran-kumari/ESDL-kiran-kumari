19cd39e1bed4a5d97980427c8c37680d3dded957
kiran-kumari <kiran.kumari1@cumminscollege.in>
2014-08-01 08:44:01 +0530

    Create calc.cpp
    This is a calculator.

---
+++ calc.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{int a=10,b=20,result=0;
+result=a+b;
+cout<<"sum of two no.s is "<<result;
+result=b-a;
+cout<<"Difference of first no. from second is "<<result;
+return 0;
+}
